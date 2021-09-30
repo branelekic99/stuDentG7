ff7a809b10606b6483ebd5b7623aa2b7265e5f59
blekic <blekic@planetsoft.ba>
2021-09-30 22:08:29 +0200

    upadte grane

--- frontend/src/pages/Requests.js
+++ frontend/src/pages/Requests.js
@@ -92,7 +92,7 @@ const Requests = () => {
     const fetchRequests = async ()=>{
         try{
             const result = await api.get("/admin/get_unapproved_requests");
-            const patientData=  await api.get("/get/patient");
+            // const patientData=  await api.get("/get/patient");
             console.log(result.data)
             setRequestData(result.data)
         }catch (err){
